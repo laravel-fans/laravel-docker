0c8e93356b6c6fa782887a5105848b5007749d6c
sink <sink.cup@gmail.com>
2021-12-10 11:35:35 +0800

    feat: #43 use php 8.1 if framework support

--- src/DockerPublishCommand.php
+++ src/DockerPublishCommand.php
@@ -13,7 +13,8 @@ class DockerPublishCommand extends Command
      *
      * @var string
      */
-    protected $signature = 'docker:publish';
+    protected $signature = 'docker:publish
+        {--php-version= : PHP version, default same as local PHP version}';
 
     /**
      * The console command description.
@@ -33,6 +34,17 @@ class DockerPublishCommand extends Command
 
         File::copyDirectory(__DIR__ . '/stubs', $basePath, FilesystemIterator::CURRENT_MODE_MASK);
 
+        // TODO: no testable, can not mock
+        $phpVersion = $this->option('php-version') ?? PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;
+        $this->replaceVersion("$basePath/Dockerfile", $phpVersion);
+
         $this->info('published successfully.');
+    }
+
+    private function replaceVersion($dockerfile, $phpVersion)
+    {
+        $content = File::get($dockerfile);
+        $content = preg_replace('/FROM php:\d\.\d/i', "FROM php:${phpVersion}", $content);
+        File::put($dockerfile, $content);
     }
 }
